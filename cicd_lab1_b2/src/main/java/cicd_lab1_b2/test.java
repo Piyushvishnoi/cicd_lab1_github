f55194f35b8ed65659cde05bf4fa4c93ecb1e5a5
Piyushvishnoi <piyushvishnoipvsh123@gmail.com>
2020-08-27 20:18:58 +0530

    my first commit

--- cicd_lab1_b2/src/main/java/cicd_lab1_b2/test.java
+++ cicd_lab1_b2/src/main/java/cicd_lab1_b2/test.java
@@ -3,7 +3,7 @@ package cicd_lab1_b2;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("testing eclipse with github");
+		System.out.println("testing eclipse with github with some change");
 
 	}
 
